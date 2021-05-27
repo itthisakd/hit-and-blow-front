269b808a431526ac1b160d4e7061093caa8b7ad6
A <itthisakdr@gmail.com>
2021-05-28 00:55:26 +0700

    data stucture finalised, tailwind setted up, components prepared, context awaiting

--- src/utilities/generateAnswer.jsx
+++ src/utilities/generateAnswer.jsx
@@ -1,17 +1,3 @@
-import React, { useState } from "react";
-import "./index.css";
-import "tailwindcss/tailwind.css";
-import Slot from "./Slot"
-import dataFormat from "./utilities/dataFormat"
-
-const color = {
-  C: "#00FFF9",
-  R: "#FF0600",
-  G: "#00FF06",
-  Y: "#FFF900",
-  P: "#F900FF",
-  B: "#0600FF",
-};
 const generateAnswers = (duplicated) => {
   let answer = {};
   do {
@@ -36,20 +22,3 @@ const generateAnswers = (duplicated) => {
   return answer;
   // generateAnswers(0) = no duplicates, generateAnswers(1) = 1 duplicated color
 };
-
-function App(props) {
-  // const pinsArr = [{ 1: "", 2: "", 3: "", 4: ""}]
-
-  const [round, setRound] = useState(1)
-  const [responses, setResponses] = useState(dataFormat);
-
-  return (
-    <div>
-      {Object.values(responses).map((response) => {
-        return <Slot pins={response[0]} />;
-      })}
-    </div>
-  );
-}
-
-export default App;
